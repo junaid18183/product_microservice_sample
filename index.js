078e010855b1552bf97a169812911ae97b52e63b
Juned Memon <junaid18183@gmail.com>
2022-05-04 17:26:45 +0530

    moved the shipment service to dedicatd microservice

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { products } = require("./fakeProductData");
-const { shipments } = require("./fakeShipmentData");
 const app = express();
 app.use(express.json());
 
@@ -16,19 +15,6 @@ app.get("/api/products/:Id", (req, res) => {
   const id = req.params.Id
   setTimeout(() => {
     res.json(products[id]);
-  }, 5);
-});
-
-app.get("/api/shipments", (req, res) => {
-  setTimeout(() => {
-    res.json(shipments);
-  }, 5);
-});
-
-app.get("/api/shipments/:Id", (req, res) => {
-  const id = req.params.Id
-  setTimeout(() => {
-    res.json(shipments[id]);
   }, 5);
 });
 
